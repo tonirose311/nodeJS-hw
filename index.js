14bf8c4951d2c100886359810c968ec42c6d4633
Tea Rose <tonidebelen2020@u.northwestern.edu>
2020-10-15 23:09:17 -0500

    adding prompts to generate questions for ReadMe

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 // array of questions for user
 const generateMarkdown = require('./utils/generateMarkdown');
-const questions = [
+// not sure how to use generate markdown at the moment
+// const questions = [
 
-];
+// ];
 
+// set up questions like activity 14 (ins_inquirer_demo)
+var inquirer = require("inquirer");
+var fs = require('fs');
 
+inquirer.prompt([
+    {
+      type: "input",
+      name: "What is the name of your Application?",
+    },
+  ]);
+  
 
 // function to write README file
-function writeToFile(fileName, data) {
-    generateMarkdown(data);
-}
+// function writeToFile(fileName, data) {
+//     // generateMarkdown(data)
+// };
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// };
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
